a816c34a77403a6dc5cf97705d582b1a46090eb6
jannes <jannes.drijkoningen@ac-systems.com>
2019-12-05 13:28:24 +0100

    feat: add opcode modes

--- day2/inc/Program.hpp
+++ day2/inc/Program.hpp
@@ -2,6 +2,28 @@
 
 #include <vector>
 
+struct Opcode {
+    int instruction;
+    int modes[3];
+
+    Opcode(int n)
+    {
+        int opcode[5] = {};
+        size_t i = 5;
+        while (n > 0)
+        {
+            int digit = n % 10;
+            n /= 10;
+            opcode[--i] = digit;
+        }
+
+        this->modes[2] = opcode[0];
+        this->modes[1] = opcode[1];
+        this->modes[0] = opcode[2];
+        this->instruction = opcode[3] * 10 + opcode[4];
+    }
+};
+
 struct Program {
     std::vector<int> initial;
     std::vector<int> state;
@@ -9,6 +31,7 @@ struct Program {
     Program(const std::vector<int> &p);
 
     int run();
+    int* getArgs(const Opcode &opcode, size_t pointer, size_t argc);
     void reset();
 };
 
@@ -17,16 +40,28 @@ Program::Program(const std::vector<int> &p) : initial(p), state(p) {}
 int Program::run()
 {
     auto &p = this->state;
+    size_t i = 0;
 
-    for (size_t i = 0; p[i] != 99; i += 4)
+    while (p[i] != 99)
     {
+        const auto oc = Opcode(p[i]);
         switch (p[i]) {
-            case 1: p[p[i + 3]] = p[p[i + 1]] + p[p[i + 2]]; break;
-            case 2: p[p[i + 3]] = p[p[i + 1]] * p[p[i + 2]]; break;
+            case 1: *getArgs(oc, i, 3) = *getArgs(oc, i, 1) + *getArgs(oc, i, 2); i += 4; break;
+            case 2: *getArgs(oc, i, 3) = *getArgs(oc, i, 1) * *getArgs(oc, i, 2); i += 4; break;
+            case 3: *getArgs(oc, i, 1) = *getArgs(oc, i, 2)                     ; i += 3; break;
+            case 4: return *getArgs(oc, i, 1); break;
             default: break;
         }
     }
     return p[0];
+}
+
+int* Program::getArgs(const Opcode &opcode, size_t pointer, size_t argc)
+{
+    auto &p = this->state;
+    return opcode.modes[argc - 1] == 0
+        ? &p[p[pointer + argc]]
+        : &p[pointer + argc];
 }
 
 void Program::reset()
---
+++ day5/src/main.cpp
@@ -0,0 +1,13 @@
+#include "util.hpp"
+#include "Program.hpp"
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        printf("Input file required\n");
+        return 1;
+    }
+    printf("Calculating Day 5\n");
+
+    return 0;
+}
