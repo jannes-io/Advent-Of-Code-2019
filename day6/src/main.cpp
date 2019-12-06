65912b5489eedf21af1dc79e041e16c156b003d3
jannes <jannes.drijkoningen@ac-systems.com>
2019-12-06 18:28:21 +0100

    feat: finished d6p1

--- day6/src/main.cpp
+++ day6/src/main.cpp
@@ -18,12 +18,16 @@ Map createLinks(std::vector<std::string> links)
     return map;
 }
 
-uint32_t countLinks(Map &map, std::string &firstId)
+uint32_t countLinks(const Map &map, const std::string &id)
 {
+    const auto it = map.find(id);
+    if (it == map.end())
+        return 0;
+
     int c = 0;
-    for (const auto &lnk : map[firstId]) {
-        
-    }
+    for (const auto &lnk : it->second)
+        c += countLinks(map, lnk) + 1;
+    return c;
 }
 
 int main (int argc, char* argv[])
@@ -36,4 +40,14 @@ int main (int argc, char* argv[])
 
     const auto input = readFile(argv[1]);
     const auto map = createLinks(input);
+
+    uint32_t c = 0;
+    for (const auto &p : map) {
+        c += countLinks(map, p.first);
+    }
+
+    printf("Part 1: %d\n", c);
+    printf("Part 1: %d\n", 0);
+
+    return 0;
 }
